af8c97a12e7ec0fcf94e06face45ac1f92aeec04
GronickGS <102252279+GronickGS@users.noreply.github.com>
2023-09-05 20:20:48 -0500

    Add files via upload

---
+++ 03 COMPRA EN RESTAURANT/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace compraRestaurante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte CANB, CANH, CANP;
+            double APAGAR;
+            const double PRECIOB = 0.8;
+            const double PRECIOH = 2;
+            const double PRECIOP = 1.2;
+
+            Console.Write("CANTIDAD DE HAMBURGUESAS :");
+            CANH = byte.Parse(Console.ReadLine());
+            Console.Write("CANTIDAD DE PAPAS :"); 
+            CANP = byte.Parse(Console.ReadLine());
+            Console.Write("CANTIDAD DE BEBIDAS :");
+            CANB = byte.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            APAGAR = (CANH * PRECIOH) + (CANP * PRECIOP) + (CANB * PRECIOB);
+            Console.WriteLine("VALOR A PAGAR: " + APAGAR);
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
