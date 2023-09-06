b781038dd9e966385521b8e95685dc64487f08d6
GronickGS <102252279+GronickGS@users.noreply.github.com>
2023-09-05 23:05:08 -0500

    Add files via upload

---
+++ 10 BONO DEL EMPLEADO POR HIJO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SUELDO, ARECIBIR;
+            int NHIJO, BONO;
+            string NOM;
+            Console.Write("NOMBRE EMPLEADO :"); 
+            NOM = Console.ReadLine();
+            Console.Write("SUELDO EMPLEADO :"); 
+            SUELDO = double.Parse(Console.ReadLine());
+            Console.Write("NÚMERO DE HIJOS :");
+            NHIJO = int.Parse(Console.ReadLine());
+            if (NHIJO >= 3) { BONO = NHIJO * 10; } else { BONO = NHIJO * 20; }
+            ARECIBIR = SUELDO + BONO;
+            Console.WriteLine("RECIBE :" + String.Format("{0:c}", ARECIBIR));
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
---
+++ 12 TARIFA TELEFÓNICA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tarifaTelefonica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CANKV;
+            double TOT, COSKV;
+            COSKV = 0;
+            Console.Write("Cantidad de Kilovatios :");
+            CANKV = int.Parse(Console.ReadLine());
+            if ((CANKV <= 1000)) COSKV = 0.14;
+            if (((CANKV > 1000) & (CANKV <= 1800))) COSKV = 0.12;
+            if ((CANKV > 1800)) COSKV = 0.8;
+            TOT = CANKV * COSKV;
+            Console.WriteLine("A PAGAR: " + TOT);
+            Console.Write("Pulse una Tecla:");
+            Console.ReadLine();
+        }
+    }
+}
