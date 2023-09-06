b979cb16c58bf3f24448de8017149319b0458825
GronickGS <102252279+GronickGS@users.noreply.github.com>
2023-09-05 20:31:24 -0500

    Add files via upload

---
+++ 06 RESTA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace resta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM1, NUM2;
+            double RESUL;
+            Console.Write("NÚMERO 1 :"); 
+            NUM1 = int.Parse(Console.ReadLine());
+            Console.Write("NÚMERO 2 :"); 
+            NUM2 = int.Parse(Console.ReadLine());
+            RESUL = (NUM1 + NUM2) * (NUM1 - NUM2);
+            Console.WriteLine();
+            Console.WriteLine("El resultado es : " + RESUL);
+            Console.ReadLine();
+        }
+    }
+}
