755fe8d8e49fca533a34fb43504fd441600c2c95
GronickGS <102252279+GronickGS@users.noreply.github.com>
2023-09-05 20:26:52 -0500

    Add files via upload

---
+++ 05 FORMATOS DE SALIDA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BASE, ALTURA, RESUL;
+      
+            Console.Write("DIGITE LA BASE :"); 
+            BASE = double.Parse(Console.ReadLine());
+            Console.Write("DIGITE LA ALTURA:"); 
+            ALTURA = double.Parse(Console.ReadLine());
+            RESUL = (BASE * ALTURA) / 2;
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:####.00}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:c}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:f}", RESUL));
+            Console.WriteLine("AREA TRIANGULO :" + String.Format("{0:g}", RESUL));
+            Console.WriteLine();
+            Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:F}", DateTime.Now));
+            Console.WriteLine("HOY ES: " + String.Format("Hoy es {0:dddd}{0:dd/MM/yyy}", DateTime.Now));
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
